28a855fddd289f434592586778b0e0f9e91e491a
kranewasd <kranehecaru@gmail.com>
2019-01-17 20:13:22 -0800

    major changes + all bugs fixed

--- sonerie.py
+++ sonerie.py
@@ -8,7 +8,6 @@ def sonerie():
     global sound,songTrigger
     now = datetime.datetime.now()
     timeNow = str(now.hour)+':'+str(now.minute)
-
     playAt = ['8:0','8:50','9:0','9:50','10:0','10:50','11:10','12:0','12:10','13:0','13:10','14:0','14:10']
     playSong = ['8:51','8:52','8:53','8:54','8:55','8:56','9:51','9:52','9:53','9:54','9:55','9:56','10:51','10:52','10:53','10:54','10:55','10:56','10:57','10:58','10:59','11:0','11:1','11:2','11:3','11:4','11:5','11:6','12:1','12:2','12:3','12:4','12:5','12:6','13:1','13:2','13:3','13:4','13:5','13:6','14:1','14:2','14:3','14:4','14:5','14:6']
     #take current day value
@@ -27,6 +26,7 @@ def sonerie():
     for i in playSong:
         if timeNow == i:
             if calendar.weekday(year,month,day) < 5:
+                print("entering melodii.py")
                 system("python "+songTrigger)
 while True:
     sleep(1/10)
